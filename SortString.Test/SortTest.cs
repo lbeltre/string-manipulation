16cfc4fb011b9b7e8efb042cdf5ff820790bd6c9
lbsilkroad <lixandro.beltre@silkroad.com>
2023-07-17 22:41:00 -0400

    ++print output

--- SortString.Test/SortTest.cs
+++ SortString.Test/SortTest.cs
@@ -1,7 +1,17 @@
+using System.Globalization;
+using Xunit.Abstractions;
+
 namespace SortString.Test
 {
     public class SortTest
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public SortTest(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void FirstScenario_Sort_String_ReturnStrigSorted()
         {
@@ -10,6 +20,8 @@ namespace SortString.Test
             var sorted = sort.SortingOperations(TestCaseData.InputFirstScenario());
 
             Assert.Equal(TestCaseData.OutputFirstScenario(), sorted);
+
+            WriteOutput("Input", string.Join(Environment.NewLine, TestCaseData.InputFirstScenario()), Environment.NewLine, "Output", string.Join(Environment.NewLine, sorted));
         }
 
         [Fact]
@@ -20,6 +32,8 @@ namespace SortString.Test
             var sorted = sort.SortingOperations(TestCaseData.InputSecondScenario());
 
             Assert.Equal(TestCaseData.OutputSecondScenario(), sorted);
+
+            WriteOutput("Input", string.Join(Environment.NewLine, TestCaseData.InputSecondScenario()), Environment.NewLine, "Output", string.Join(Environment.NewLine, sorted));
         }
 
         [Fact]
@@ -30,6 +44,7 @@ namespace SortString.Test
             var sorted = sort.SortingOperations(TestCaseData.InputThirdScenario());
 
             Assert.Equal(TestCaseData.OutputThirdScenario(), sorted);
+            WriteOutput("Input", string.Join(Environment.NewLine, TestCaseData.InputThirdScenario()), Environment.NewLine, "Output", string.Join(Environment.NewLine, sorted));
         }
 
         [Fact]
@@ -40,6 +55,7 @@ namespace SortString.Test
             var sorted = sort.SortingOperations(TestCaseData.InputFourthScenario());
 
             Assert.Equal(TestCaseData.OutputFourthScenario(), sorted);
+            WriteOutput("Input", string.Join(Environment.NewLine, TestCaseData.InputFourthScenario()), Environment.NewLine, "Output", string.Join(Environment.NewLine, sorted));
         }
 
         [Fact]
@@ -50,6 +66,7 @@ namespace SortString.Test
             var sorted = sort.SortingOperations(TestCaseData.InputFifthScenario());
 
             Assert.Equal(TestCaseData.OutputFifthScenario(), sorted);
+            WriteOutput("Input", string.Join(Environment.NewLine, TestCaseData.InputFifthScenario()), Environment.NewLine, "Output", string.Join(Environment.NewLine, sorted));
         }
 
         [Fact]
@@ -60,6 +77,13 @@ namespace SortString.Test
             var sorted = sort.SortingOperations(TestCaseData.InputSixthScenario());
 
             Assert.Equal(TestCaseData.OutputSixthScenario(), sorted);
+            WriteOutput("Input", string.Join(Environment.NewLine, TestCaseData.InputSixthScenario()), Environment.NewLine, "Output", string.Join(Environment.NewLine, sorted));
+        }
+
+        void WriteOutput(params string[] data)
+        {
+            foreach (var item in data)
+                testOutputHelper.WriteLine(item);
         }
     }
 }
